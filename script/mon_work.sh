385e0c0ddfe51e1ca9da09e4d31bc2469e8dfc82
Jockesan <lordigoo@gmail.com>
2018-04-06 23:16:04 +0200

    ..

--- script/mon_work.sh
+++ script/mon_work.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 xrandr --output DP-1-1 --rotate left --right-of DP-1-2 --output DP-2 --rotate normal --left-of DP-1-2
-notify-send -t 4 "Work Work" "Monitor setup -> Work"
+notify-send -t 10 "Work Work" "Monitor setup -> Work"
 
