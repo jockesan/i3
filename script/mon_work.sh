e2505dc9014478d7da904ebbfe45c13114a7f3d5
Jockesan <lordigoo@gmail.com>
2018-04-12 18:53:36 +0200

    ¯\_(ツ)_/¯

--- script/mon_work.sh
+++ script/mon_work.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+notify-send -t 5 "Changing settings in 5" sleep 5
 xrandr --output DP-1-1 --rotate left --right-of DP-1-2 --output DP-2 --rotate normal --left-of DP-1-2
 notify-send -t 10 "Work Work" "Monitor setup -> Work"
 
