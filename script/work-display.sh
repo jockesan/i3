f4f930841a482330bad722ace29229d46ccf5647
Jockesan <lordigoo@gmail.com>
2018-08-07 08:11:38 +0200

    ¯\_(ツ)_/¯

--- script/work-display.sh
+++ script/work-display.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Fix monitors at work
 
 xrandr --output DP-1-1 --rotate left --right-of DP-1-2 --output DP-2 --rotate normal --left-of DP-1-2
--- script/xscreen-lock.sh
+++ script/xscreen-lock.sh
@@ -1,3 +1,2 @@
-
-
+#!/bin/bash
 xscreensaver-command --lock
